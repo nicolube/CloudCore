b49591241b793cf35e070c25be9899413630deb9
Nico Lube <dev@nicolube.de>
2020-04-27 00:28:42 +0200

    Fixing the Event based executer!

--- LightFallCorePlugin/src/main/java/de/lightfall/core/bukkit/EventBasedExecutions.java
+++ LightFallCorePlugin/src/main/java/de/lightfall/core/bukkit/EventBasedExecutions.java
@@ -1,6 +1,5 @@
 package de.lightfall.core.bukkit;
 
-import de.lightfall.core.api.channelhandeler.documents.Document;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventPriority;
@@ -13,7 +12,7 @@ import java.util.*;
 public class EventBasedExecutions implements Listener {
 
     private final MainBukkit plugin;
-    private final Map<Class, List<EventExecutorTask>> executionMap;
+    private final Map<Class<? extends Event>, List<EventExecutorTask>> executionMap;
     private final HashSet<Class<? extends Event>> registeredEvents;
     private final EventExecutor executor;
     private final Listener dummyListener;
@@ -24,33 +23,34 @@ public class EventBasedExecutions implements Listener {
         this.registeredEvents = new HashSet<>();
         this.dummyListener = new Listener() {};
 
-        this.executor = (listener, event) -> {
-            this.executionMap.entrySet().removeIf(classListEntry -> {
-                if (event.getClass().equals(classListEntry.getKey())) {
-                    classListEntry.getValue().forEach(executorTask -> executorTask.execute(event));
-                    return true;
-                }
-                return false;
-            });
-        };
+        this.executor = (listener, event) ->
+                this.executionMap.entrySet().removeIf(classListEntry -> {
+                    if (event.getClass().equals(classListEntry.getKey())) {
+                        classListEntry.getValue().removeIf(executorTask -> executorTask.execute(event));
+                        return classListEntry.getValue().isEmpty();
+                    }
+                    return false;
+                });
     }
 
     public void scheduleExecution(EventExecutorTask executor) {
-        final Class<? extends Event> clazz = (Class) ((ParameterizedType) executor.getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0];
-        if (!registeredEvents.contains(clazz)) {
+        final Class<? extends Event> clazz = (Class<? extends Event>) ((ParameterizedType) executor.getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0];
+        if (!this.registeredEvents.contains(clazz)) {
             Bukkit.getPluginManager().registerEvent(clazz, dummyListener, EventPriority.NORMAL, this.executor, this.plugin);
+            this.registeredEvents.add(clazz);
         }
         List<EventExecutorTask> executors = this.executionMap.get(clazz);
         if (executors == null) {
             executors = new ArrayList<>();
             executors.add(executor);
             this.executionMap.put(clazz, executors);
+            return;
         }
         executors.add(executor);
         this.executionMap.put(clazz, executors);
     }
 
-    public interface EventExecutorTask <E extends Event>{
-        public void execute(E event);
+    public interface EventExecutorTask<E extends Event> {
+        public boolean execute(E event);
     }
 }
