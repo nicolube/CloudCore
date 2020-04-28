ecb9adcf6815f12b64d4f3eacd485747c4af4906
Anton Bowe <anton.bowe@meinaxia.net>
2020-04-28 22:27:21 +0200

    Added KickReason and KickDocument

--- LightFallCoreAPI/src/main/java/de/lightfall/core/api/channelhandeler/DocumentRegister.java
+++ LightFallCoreAPI/src/main/java/de/lightfall/core/api/channelhandeler/DocumentRegister.java
@@ -1,5 +1,6 @@
 package de.lightfall.core.api.channelhandeler;
 
+import de.lightfall.core.api.channelhandeler.documents.KickDocument;
 import de.lightfall.core.api.channelhandeler.documents.LocationDocument;
 import de.lightfall.core.api.channelhandeler.documents.TeleportDocument;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.Getter;
 @Getter
 public enum  DocumentRegister {
     TELEPORT(TeleportDocument.class),
+    KICK(KickDocument.class),
     LOCATION(LocationDocument.class);
 
     private Class clazz;
